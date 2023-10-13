8ddebd9de520f5736983355e8e0dee7ffdc109e1
Henrik Hagfeldt <henke9606@hotmail.com>
2023-10-13 20:59:28 +0200

    Changing background color

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class BrainKickApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(0, 255, 122, 122)),
         ),
         home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 255, 0, 0),
+          backgroundColor: Color.fromARGB(255, 31, 31, 31),
           body: GameScreen(),
         )
       ),
